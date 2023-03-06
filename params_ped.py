60f626e550213925b5fa0837dc649cfc944ec231
Patrick Meier <patrick-pii@hotmail.com>
2023-03-06 15:58:20 +0100

    updated params file for docker use

--- params_ped.py
+++ params_ped.py
@@ -6,39 +6,40 @@ Created on Mon Jan 16 10:02:12 2023
 @author: rafael
 """
 
-#%% General settings
+### General
 form_id = 'ped'
 form_title = 'Ped'
-htmlcontent = True # whether the diagram  contains html in the content of objects (content is in the diagram)
-htmlfolder = '/home/rafael/Documents/git/almsom/html/' # folder where the html files are stored (that will replace the text in notes)
+htmlcontent = True # whether the diagram  contains html in the content of objects
 platform = 'cht'
 testing = False
 context_params_from_facility=True
+mhsplit = False  # whether several nodes are combined into 1 (multiheadline split)
+interrupt_flow = True
+activity = 'treatment'
 
+### Input Streamlit webapp folder
+folder = '/app/'
+resource_folder = 'resources/'
+inputfile_dx = folder+'uploaded_files/dx.drawio'  # inputfile for jupyter notebook diagnostic
+inputfile = folder+'uploaded_files/tt.drawio' # inputfile for tricc-graph tt
+diagnosis_order = folder+'uploaded_files/diagnosis_order.csv'
+input_trans = folder+'uploaded_files/ped_fr.xlsx'
 
-#%% Form specific input files and settings
-folder = '/home/rafael/Documents/git/MSFeCARE/forms-clinical/ped/release20230221/'
-inputfile_dx = folder+'dx.drawio'  # diagnostic inputfile for legacy notebook (currently AlmLib, MSF PED, MSF YI)
-inputfile = folder+'tt.drawio' # inputfile for tricc-graph tt
-activity = 'treatment' # activity that should be built from inputfile (for MSF there is only 'treatment')
-input_trans = folder + 'ped_fr.xlsx' # file with translated strings
-diagnosis_order = folder+'diagnosis_order.csv' # hierarchy of diagnosis
-drugsfile = 'medications_zscores.xlsx' # global medication lookup
-cafile = 'ca.xlsx' # Table for Caretaker advice message logic
-summaryfile = 'summary.xlsx' # Template for the summary
-interrupt_flow = True # whether the form has pauses and uses CHT tasks
-mhsplit = False  # multiheadline split: whether several nodes are combined into 1 object in the diagram
+### Resources
+cafile = resource_folder+'ca.xlsx'
+summaryfile = resource_folder+'summary.xlsx'
 
-
-#%% output files made by TRICC
+### Output
 output_folder = folder + 'output_for_zip/' # folder where output files will be stored
-media_folder = output_folder+'media/images/'
+output_xls = folder + 'tt.xlsx'
 output = output_folder + form_id + '.xlsx'
+media_folder = output_folder+'media/images/'
+updated_trans = output_folder+'ped_fr_newest.xlsx'
 zipfile = folder + 'output/output'
 
-
-#%% TRICC internal settings & parameters
-output_xls = folder + 'tt.xlsx'  # where to write the TT part
-updated_trans = output_folder + 'ped_fr_newest.xlsx' # where to save updated translations
-headerfile = folder+'formheader_cht.xlsx'  # Formheader for the base form
-headerfile_pause = folder+'formheader_cht_pause.xlsx' # Formheader for the CHT tasks form
+### Input TRICC Repo folder
+repo_folder = '/app/TRICC_REPO/TRICC-Webapp/'
+drugsfile = repo_folder+'medications_zscores.xlsx'
+headerfile = repo_folder+'formheader_cht.xlsx'
+headerfile_pause = repo_folder+'formheader_cht_pause.xlsx'
+breakpoints = repo_folder+'breakpoints.csv'
