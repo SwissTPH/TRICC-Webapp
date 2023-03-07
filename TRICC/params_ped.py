5f41c5faa8217a58728468b85f31d768ac10cb7b
RafaelKluender <rafael.kluender@swisstph.ch>
2023-03-07 09:18:44 +0100

    minor file edits

--- TRICC/params_ped.py
+++ TRICC/params_ped.py
@@ -6,7 +6,7 @@ Created on Mon Jan 16 10:02:12 2023
 @author: rafael
 """
 
-### Genera
+### General
 form_id = 'ped'
 form_title = 'Ped'
 htmlcontent = True # whether the diagram  contains html in the content of objects
