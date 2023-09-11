776187ee7133e82aea1920ebc7698fa463e24519
Martin Norris <43757408+koanic@users.noreply.github.com>
2023-09-11 10:09:48 +0200

    Update params_ped.py
    Changed interrupt flow to true to enable after pause form to be created

--- TRICC/params_ped.py
+++ TRICC/params_ped.py
@@ -16,7 +16,7 @@ platform = 'cht'
 testing = False
 context_params_from_facility=True
 mhsplit = False  # whether several nodes are combined into 1 (multiheadline split)
-interrupt_flow = False
+interrupt_flow = True
 activity = 'treatment'
 
 ### Input Streamlit webapp folder
